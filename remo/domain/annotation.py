03b2d8da1aead64a9a1cd37792c6f38fea0aa637
Andrea <larosa.andre@gmail.com>
2020-03-17 15:44:27 +0000

    added __str__ for Annotation

--- remo/domain/annotation.py
+++ remo/domain/annotation.py
@@ -34,7 +34,18 @@ class Annotation:
         self.classes = classes if isinstance(classes, list) else [classes]
         self.object = object
         self.coordinates = None
+        self.type = None
+        
+    def __str__(self):
+        
+        my_representation = "Annotation: {classes} ".format(classes=self.classes)
+        
+        my_representation = my_representation + "(type:{ann_type}, file:{filename})".format(ann_type=self.type, filename= self.img_filename) 
+            
+        return my_representation
 
+    def __repr__(self):
+        return self.__str__()
 
     @property
     def task(self):
@@ -55,6 +66,7 @@ class Annotation:
 
         self.object = Annotation.Bbox(*values)
         self.coordinates = values
+        self.type = 'Bounding Box'
         
     @property
     def segment(self):
@@ -72,6 +84,7 @@ class Annotation:
             )
         self.object = Annotation.Segment(points)
         self.coordinates = points
+        self.type = 'Polygon'
         
     
     class Bbox:
