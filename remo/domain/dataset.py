0d091f7ec5717fdddc6f1c19cafff83f6ca02bf9
Andrea La Rosa <larosa.andre@gmail.com>
2020-07-22 17:44:27 +0100

    thousands sep for image count

--- remo/domain/dataset.py
+++ remo/domain/dataset.py
@@ -28,7 +28,7 @@ class Dataset:
 
     def __str__(self):
         
-        return "Dataset {id:2d} - {name:5s} - {n_images} images".format(id=self.id, name="'" + self.name + "'", n_images = self.n_images)
+        return "Dataset {id:2d} - {name:5s} - {n_images:,} images".format(id=self.id, name="'" + self.name + "'", n_images = self.n_images)
 
     def __repr__(self):
         return self.__str__()
