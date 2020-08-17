c33cfa84ba75e049bd940b2b0703dbc7b866537b
Andrea La Rosa <larosa.andre@gmail.com>
2020-08-17 16:31:58 +0100

    gitignore and updated example for add_data

--- remo/domain/dataset.py
+++ remo/domain/dataset.py
@@ -79,9 +79,12 @@ Annotation Sets: {n_annotation_sets}""".format(
         - Adding archive files: support for ``zip``, ``tar``, ``gzip``
 
         Example::
+            ! wget 'https://remo-scripts.s3-eu-west-1.amazonaws.com/open_images_sample_dataset.zip'
+            ! unzip open_images_sample_dataset.zip
+            
             urls = ['https://remo-scripts.s3-eu-west-1.amazonaws.com/open_images_sample_dataset.zip']
-            my_dataset = remo.create_dataset(name = 'D1', urls = urls)
-            my_dataset.add_data(local_files=annotation_files, annotation_task = 'Object detection')
+            my_dataset = remo.create_dataset(name = 'D1')
+            my_dataset.add_data(local_files=['./open_images_sample_dataset'], annotation_task = 'Object detection')
 
         Args:
             dataset_id: id of the dataset to add data to
